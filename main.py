27eeb8a3d92a8d074d1666ca43174d02c450f069
LXL\Luna <lunaxli@g.ucla.edu>
2017-06-18 23:12:28 -0700

    Added variable to choose specific model(s) to run
    Also added verbose keyword

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.classification import DecisionTreeClassifier
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.classification import GBTClassifier
-from pyspark.ml.classification import MultilayerPerceptronClassifier
 from pyspark.ml.classification import NaiveBayes
 import csv
+
+### model(s) to run 
+### To run all, leave empty. 
+### To run specific models include "lr", "dtc", "rfc", "gbt", "nb")
+MODEL = set([])
+###
+
+### flags
+VERBOSE = False
+###
 
 ### path to SPARK_HOME
 KEVIN_HOME = ""
@@ -61,67 +70,77 @@ with open(logFile, "w") as log:
     test = sqlContext.createDataFrame(testData, ["label", "name1", "name2", "features"])
 
     with open(resultFile, "w") as results:
-        results.write('Logistic Regression\n\n')
-        lr = LogisticRegression(maxIter=10, regParam=0.01)
-        log.write("LogisticRegression params:\n" + str(lr.explainParams()) + "\n")
-        model = lr.fit(training)
-        log.write("Model was fit using params: ")
-        log.write(str(model.extractParamMap())) # this may not be working, not sure if str cast works haha
-        prediction = model.transform(test)
-        res = prediction.select("features", "label", "probability", "prediction").rdd
-        acc = res.filter(lambda (f, label, prob, pred): label == pred).count() / float(testData.count())
-        results.write('Accuracy: ' + str(acc) + '\n\n')
-        r = res.map(lambda row: "features=%s, label=%s -> prob=%s, prediction=%s\n" % (row.features, row.label, row.probability, row.prediction))
-        for line in r.collect():
-            results.write(line)
+        if(not MODEL or "lr" in MODEL):
+            results.write('Logistic Regression\n\n')
+            lr = LogisticRegression(maxIter=10, regParam=0.01)
+            log.write("LogisticRegression params:\n" + str(lr.explainParams()) + "\n")
+            model = lr.fit(training)
+            log.write("Model was fit using params: ")
+            log.write(str(model.extractParamMap())) # this may not be working, not sure if str cast works haha
+            prediction = model.transform(test)
+            res = prediction.select("features", "label", "probability", "prediction").rdd
+            acc = res.filter(lambda (f, label, prob, pred): label == pred).count() / float(testData.count())
+            results.write('Accuracy: ' + str(acc) + '\n\n')
+            r = res.map(lambda row: "features=%s, label=%s -> prob=%s, prediction=%s\n" % (row.features, row.label, row.probability, row.prediction))
+            if(VERBOSE):
+                for line in r.collect():
+                    results.write(line)
         
-        results.write('\n\n------------------------------------------------------\n\n')
-        results.write('Decision Tree Classifier\n\n')
-        dt = DecisionTreeClassifier(labelCol="label", featuresCol="features")
-        model = dt.fit(training)
-        prediction = model.transform(test)
-        res = prediction.select("features", "label", "probability", "prediction").rdd
-        acc = res.filter(lambda (f, label, prob, pred): label == pred).count() /float(testData.count())
-        results.write('Accuracy: ' + str(acc) + '\n\n')
-        r = res.map(lambda row: "features=%s, label=%s -> prob=%s, prediction=%s\n" % (row.features, row.label, row.probability, row.prediction))
-        for line in r.collect():
-            results.write(line)   
+        if(not MODEL or "dtc" in MODEL):
+            results.write('\n\n------------------------------------------------------\n\n')
+            results.write('Decision Tree Classifier\n\n')
+            dt = DecisionTreeClassifier(labelCol="label", featuresCol="features")
+            model = dt.fit(training)
+            prediction = model.transform(test)
+            res = prediction.select("features", "label", "probability", "prediction").rdd
+            acc = res.filter(lambda (f, label, prob, pred): label == pred).count() /float(testData.count())
+            results.write('Accuracy: ' + str(acc) + '\n\n')
+            r = res.map(lambda row: "features=%s, label=%s -> prob=%s, prediction=%s\n" % (row.features, row.label, row.probability, row.prediction))
+            if(VERBOSE):
+                for line in r.collect():
+                    results.write(line)   
 
-        results.write('\n\n------------------------------------------------------\n\n')
-        results.write('Random Forest Classifier\n\n')
-        rf = RandomForestClassifier(labelCol="label", featuresCol="features", numTrees=10)
-        model = rf.fit(training)
-        prediction = model.transform(test)
-        res = prediction.select("features", "label", "probability", "prediction").rdd
-        acc = res.filter(lambda (f, label, prob, pred): label == pred).count() /float(testData.count())
-        results.write('Accuracy: ' + str(acc) + '\n\n')
-        r = res.map(lambda row: "features=%s, label=%s -> prob=%s, prediction=%s\n" % (row.features, row.label, row.probability, row.prediction)) 	
-        for line in r.collect():
-            results.write(line)
+        if(not MODEL or "rfc" in MODEL):
+            results.write('\n\n------------------------------------------------------\n\n')
+            results.write('Random Forest Classifier\n\n')
+            rf = RandomForestClassifier(labelCol="label", featuresCol="features", numTrees=10)
+            model = rf.fit(training)
+            prediction = model.transform(test)
+            res = prediction.select("features", "label", "probability", "prediction").rdd
+            acc = res.filter(lambda (f, label, prob, pred): label == pred).count() /float(testData.count())
+            results.write('Accuracy: ' + str(acc) + '\n\n')
+            r = res.map(lambda row: "features=%s, label=%s -> prob=%s, prediction=%s\n" % (row.features, row.label, row.probability, row.prediction)) 	
+            if(VERBOSE):
+                for line in r.collect():
+                    results.write(line)
 
-        results.write('\n\n------------------------------------------------------\n\n')
-        results.write('Gradient Boosted Tree Classifier\n\n')
-        gbt = GBTClassifier(labelCol="label", featuresCol="features", maxIter=10)
-        model = gbt.fit(training)
-        prediction = model.transform(test)
-        res = prediction.select("features", "label", "prediction").rdd
-        acc = res.filter(lambda (f, label, pred): label == pred).count() /float(testData.count())
-        results.write('Accuracy: ' + str(acc) + '\n\n')
-        r = res.map(lambda row: "features=%s, label=%s -> prediction=%s\n" % (row.features, row.label, row.prediction))
-        for line in r.collect():
-            results.write(line)
+        if(not MODEL or "gbt" in MODEL):
+            results.write('\n\n------------------------------------------------------\n\n')
+            results.write('Gradient Boosted Tree Classifier\n\n')
+            gbt = GBTClassifier(labelCol="label", featuresCol="features", maxIter=10)
+            model = gbt.fit(training)
+            prediction = model.transform(test)
+            res = prediction.select("features", "label", "prediction").rdd
+            acc = res.filter(lambda (f, label, pred): label == pred).count() /float(testData.count())
+            results.write('Accuracy: ' + str(acc) + '\n\n')
+            r = res.map(lambda row: "features=%s, label=%s -> prediction=%s\n" % (row.features, row.label, row.prediction))
+            if(VERBOSE):
+                for line in r.collect():
+                    results.write(line)
         
-        results.write('\n\n------------------------------------------------------\n\n')
-        results.write('Naive Bayes\n\n')
-        nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
-        model = nb.fit(training)
-        prediction = model.transform(test)
-        res = prediction.select("features", "label", "probability", "prediction").rdd
-        acc = res.filter(lambda (f, label, prob, pred): label == pred).count() /float(testData.count())
-        results.write('Accuracy: ' + str(acc) + '\n\n')
-        r = res.map(lambda row: "features=%s, label=%s -> prob=%s, prediction=%s\n" % (row.features, row.label, row.probability, row.prediction))
-        for line in r.collect():
-            results.write(line)
+        if(not MODEL or "nb" in MODEL):
+            results.write('\n\n------------------------------------------------------\n\n')
+            results.write('Naive Bayes\n\n')
+            nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
+            model = nb.fit(training)
+            prediction = model.transform(test)
+            res = prediction.select("features", "label", "probability", "prediction").rdd
+            acc = res.filter(lambda (f, label, prob, pred): label == pred).count() /float(testData.count())
+            results.write('Accuracy: ' + str(acc) + '\n\n')
+            r = res.map(lambda row: "features=%s, label=%s -> prob=%s, prediction=%s\n" % (row.features, row.label, row.probability, row.prediction))
+            if(VERBOSE):
+                for line in r.collect():
+                    results.write(line)
 
         '''
         results.write('\n\n------------------------------------------------------\n\n')
